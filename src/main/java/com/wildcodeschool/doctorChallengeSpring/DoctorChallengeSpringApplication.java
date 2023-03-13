3feb97205cc1833776929d43ef6fd05ee7bb73ed
jimboux <100678513+jimboux@users.noreply.github.com>
2023-03-13 15:07:35 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/doctorChallengeSpring/DoctorChallengeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorChallengeSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorChallengeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorChallengeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/doctorChallengeSpring/DoctorChallengeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.doctorChallengeSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorChallengeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
